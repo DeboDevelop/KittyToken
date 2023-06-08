c511c7c1c82d98b1b97d5f42aa3a13de29032681
DeboDevelop <debajyotidofficial@gmail.com>
2023-06-08 20:24:39 +0530

    feat: Add max supply

--- contracts/KittyToken.sol
+++ contracts/KittyToken.sol
@@ -3,11 +3,12 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts@4.9.1/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.1/token/ERC20/extensions/ERC20Capped.sol";
 
-contract KittyToken is ERC20 {
+contract KittyToken is ERC20Capped {
     address payable public owner;
-    constructor(uint256 intialSupply) ERC20("KittyToken", "KYT") {
+    constructor(uint256 cap) ERC20("KittyToken", "KYT") ERC20Capped(cap * (10 ** decimals())) {
         owner = msg.sender
-        _mint(owner, intialSupply);
+        _mint(owner, 70000 * (10 ** decimals()));
     }
 }
