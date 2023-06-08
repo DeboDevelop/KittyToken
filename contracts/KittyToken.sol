f0a5e1ef513e12e33f5975803ecf415f876390b5
DeboDevelop <debajyotidofficial@gmail.com>
2023-06-08 23:07:42 +0530

    feat: Add block reward for miner

--- contracts/KittyToken.sol
+++ contracts/KittyToken.sol
@@ -2,14 +2,45 @@
 
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts@4.9.1/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.9.1/token/ERC20/extensions/ERC20Capped.sol";
-import "@openzeppelin/contracts@4.9.1/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract KittyToken is ERC20Capped, ERC20Burnable {
     address payable public owner;
-    constructor(uint256 cap) ERC20("KittyToken", "KYT") ERC20Capped(cap * (10 ** decimals())) {
-        owner = msg.sender
+    uint256 public blockReward;
+    constructor(uint256 cap, uint256 reward) ERC20("KittyToken", "KYT") ERC20Capped(cap * (10 ** decimals())) {
+        owner = payable(msg.sender);
         _mint(owner, 70000 * (10 ** decimals()));
+        blockReward = reward * (10 ** decimals());
+    }
+
+    function _mint(address account, uint256 amount) internal override(ERC20Capped, ERC20) {
+        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+        super._mint(account, amount);
+    }
+
+    function setBlockReward(uint256 reward) public onlyOwner {
+        blockReward = reward * (10 ** decimals());
+    }
+
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, blockReward);
+    }
+
+    function _beforeTokenTransfer(address from, address to,uint256 value) internal override {
+        if (from != address(0) && to != address(0) && to != block.coinbase) {
+            _mintMinerReward();
+        }
+        super._beforeTokenTransfer(from, to, value);
+    }
+
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only Owner can call this function");
+        _;
     }
 }
