91eb17e36c28b7e61ec049f63b927c8118b1f75c
DeboDevelop <debajyotidofficial@gmail.com>
2023-06-09 15:13:18 +0530

    feat: Add Deployment script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    goerli: {
+      url: process.env.INFURA_GOERLI_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
